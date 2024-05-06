83f7d9a11b1b8bfb150f7b7fae6c8f8aa402c053
Vergil Wong <s644906481@gmail.com>
2024-05-06 21:10:58 +0800

    Feat: make Gogo gogo again

--- bot.py
+++ bot.py
@@ -14,7 +14,13 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
+    print("Received a message: ", message.text)
     replies = ["我刚刚尿沙发上了", "我刚刚尿万里床上了", "我刚刚尿安德床上了", "我刚刚尿老王床上了"]
     bot.reply_to(message, random.choice(replies))
 
+@bot.message_handler(func=lambda message: message.text and '@' + bot.get_me().username.lower() in message.text.lower())
+def handle_mention(message):
+    responses = ["啊", "啊啊", "啊啊啊"]
+    bot.reply_to(message, random.choice(responses))
+    
 bot.infinity_polling()
